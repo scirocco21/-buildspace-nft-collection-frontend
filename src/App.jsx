8cf5ef83bd4f6a2955792b01151932a29efad44d
Seb Gertz <sebgertz@gmail.com>
2021-12-17 03:06:00 +0000

    feat: alert users when they're not on rinkeby

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,14 @@ const App = () => {
     * First make sure we have access to window.ethereum
     */
     const { ethereum } = window;
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+    console.log("Connected to chain " + chainId);
+
+      // String, hex code of the chainId of the Rinkebey test network
+    const rinkebyChainId = "0x4"; 
+    if (chainId !== rinkebyChainId) {
+      alert("You are not connected to the Rinkeby Test Network!");
+    }
 
     if (!ethereum) {
       console.log("Make sure you have metamask!");
@@ -51,7 +59,14 @@ const App = () => {
   const connectWallet = async () => {
     try {
       const { ethereum } = window;
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
 
+        // String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4"; 
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
       if (!ethereum) {
         alert("Get MetaMask!");
         return;
@@ -75,6 +90,7 @@ const App = () => {
 
 const askContractToMintNft = async () => {
   try {
+    
     const { ethereum } = window;
 
     if (ethereum) {
